506658154fbe3c5819b0a990b6780722393346a3
“knakaji” <knakaji@mac.titech.ac.jp>
2026-04-29 22:38:05 +0900

    260429

--- chap04_ode/Maxwell_sinuStrain.py
+++ chap04_ode/Maxwell_sinuStrain.py
@@ -34,16 +34,16 @@ try:
 except ValueError:
     eamp = 0.2
 try:
-    f = float(input('frequency for sinusoidal strain [Hz] (default=0.5 Hz): '))
+    freq = float(input('frequency for sinusoidal strain [Hz] (default=1.0 Hz): '))
 except ValueError:
-    f = 0.5
+    freq = 1.0
 
-af = 2*np.pi*f
+af = 2*np.pi*freq
 
 s0 = 0                      # [] initial strain
 
-tmax = 4/f                  # [s] duration time
-dt = 1/(40*f)               # [s] interval time
+tmax = 4/freq                   # [s] duration time
+dt = 1/(40*freq)                # [s] interval time
 t_a = np.arange(0, tmax, dt)    # time after step stress
 t_b = np.arange(-0.1*tmax,0,dt) # time before step stress
 t = np.concatenate([t_b,t_a])   # whole time 
@@ -77,7 +77,7 @@ ax2.set_xlim(t[0], t[-1])
 ax1.set_ylim(-1.5*np.max(e),1.5*np.max(e))
 ax2.set_ylim(-1.5*np.max(s),1.5*np.max(s))
 
-var_text = r'$\epsilon_{{amp}}$ = {0:.2f}, $f$ = {1:.3f} Hz, $E$ = {2:.1f} MPa, $\eta$ = {3:.1f} kPa s'.format(eamp,f,E/10**6,eta/10**3)
+var_text = r'$\epsilon_{{amp}}$ = {0:.2f}, $f$ = {1:.3f} Hz, $E$ = {2:.1f} MPa, $\eta$ = {3:.1f} kPa s'.format(eamp,freq,E/10**6,eta/10**3)
 ax1.text(0.1, 0.9, var_text, transform=ax1.transAxes)
 eq_text = r'd$\sigma$/d$t$ = -$\sigma$/$\tau$ + $E$d$\epsilon$/d$t$'
 ax2.text(0.1, 0.9, eq_text, transform=ax2.transAxes)
@@ -105,7 +105,7 @@ def animate(i):
 # アニメーション実行
 ani = animation.FuncAnimation(fig, animate, frames=len(t), interval=20, blit=True)
 
-savefile = "./gif/Maxwell_sinuStrain_(f={0:.3f}Hz).gif".format(f)
+savefile = "./gif/Maxwell_sinuStrain_(f={0:.3f}Hz).gif".format(freq)
 ani.save(savefile, writer='pillow', fps=50)
 
 plt.show()
