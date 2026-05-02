2bd80197b3254f4b0d1aefa06ed9b948a3a0a525
“knakaji” <knakaji@mac.titech.ac.jp>
2026-05-02 17:58:51 +0900

    260502

--- chap03/Boltzmann_superposition_relax_stepStrain.py
+++ chap03/Boltzmann_superposition_relax_stepStrain.py
@@ -10,20 +10,28 @@ def relaxation_modulus(t_elapsed, E_inf, E_i, tau):
 
 # 1. データ準備
 strain_i = 1.0  # ステップ歪みの大きさ
-t = np.linspace(-2, 10, 400)
-t1, t2 = 0.0, 2.0  # ステップ歪みを加える時刻
-E_inf = 0.5     # 平衡弾性率
-E_i = 3.0       # 瞬間弾性率
-tau = 2.0       # 緩和時間
+start_time = -2.0  # 開始時間
+end_time = 8.0    # 終了時間
+time_duration = end_time - start_time  # [s]
+fps = 30
+steps = int(time_duration * fps) + 1
+interval_ms = 1000 / fps  # 1コマあたりのミリ秒
+t = np.linspace(start_time, end_time, steps)
+t1, t2 = 0.0, 2.0   # ステップ歪みを加える時刻
+E_inf = 0.5         # 平衡弾性率
+E_i = 3.0           # 瞬間弾性率
+tau = 2.0           # 緩和時間
 stress1 = strain_i * relaxation_modulus(t - t1, E_inf, E_i, tau)
 stress2 = strain_i * relaxation_modulus(t - t2, E_inf, E_i, tau)
 stress_total = stress1 + stress2
+
+strain = np.where(t < t1, 0, np.where(t < t2, 1.0, 2.0))
 
 # 2. グラフの初期設定
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 7), sharex=True)
 
 # --- 上段：歪み (Input) ---
-ax1.set_xlim(-2, 10)
+ax1.set_xlim(start_time, end_time)
 ax1.set_ylim(-0.2, 3) # 縦軸を固定
 ax1.set_ylabel('Applied Strain, $\epsilon$ /')
 ax1.set_title('Boltzmann Superposition (Strees Relaxation) ($E_i$ = {0:.1f} MPa, $E_\\infty$ = {1:.1f} MPa, $\\tau$ = {2:.1f} s)'.format(E_i, E_inf, tau))
@@ -32,7 +40,7 @@ ax1.grid(True, ls='--')
 line_strain, = ax1.step([], [], where='post', color='blue', lw=2)
 
 # --- 下段：応力 (Response) ---
-ax2.set_xlim(-2, 10)
+ax2.set_xlim(start_time, end_time)
 ax2.set_ylim(-0.2, 5.0) # 縦軸を固定
 ax2.set_xlabel('Time /s')
 ax2.set_ylabel('Stress, $\sigma$ /MPa')
@@ -45,10 +53,8 @@ ax2.legend(loc='upper right')
 
 # 3. アニメーション更新関数
 def animate(i):
-    curr_t = t[:i]
-    # 歪みデータの更新
-    strain_history = np.where(curr_t < t1, 0, np.where(curr_t < t2, 1.0, 2.0))
-    line_strain.set_data(curr_t, strain_history)
+     # 歪みデータの更新
+    line_strain.set_data(t[:i], strain[:i])
     
     # 応力データの更新
     line_s1.set_data(t[:i], stress1[:i])
@@ -57,9 +63,10 @@ def animate(i):
     return line_strain, line_s1, line_s2, line_total
 
 # アニメーション実行
-ani = animation.FuncAnimation(fig, animate, frames=len(t), interval=25, blit=True)
+ani = animation.FuncAnimation(fig, animate, frames=steps, interval=interval_ms, blit=True)
 
-ani.save('./gif/Boltzmann_relax_stepStrain.gif', dpi=300)
+savefile = './mp4/Boltzmann_relax_stepStrain.mp4'
+ani.save(savefile, writer='ffmpeg', fps=fps, extra_args=['-r', '30'])
 
 plt.tight_layout()
 plt.show()
