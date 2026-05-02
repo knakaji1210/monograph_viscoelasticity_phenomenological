97231894121d8475932449a53485bbd789596d9e
“knakaji” <knakaji@mac.titech.ac.jp>
2026-05-02 18:10:09 +0900

    260502

--- chap03/Boltzmann_superposition_relax_pulseStrain.py
+++ chap03/Boltzmann_superposition_relax_pulseStrain.py
@@ -10,27 +10,35 @@ def relaxation_modulus(t_elapsed, E_inf, E_i, tau):
 
 # 1. データ準備
 strain_i = 1.0  # ステップ歪みの大きさ
-t = np.linspace(-2, 10, 400)
-t_on, t_off = 0.0, 4.0  # ステップ歪みを加える時刻
-E_inf = 0.5     # 平衡弾性率
-E_i = 3.0       # 瞬間弾性率
-tau = 2.0       # 緩和時間
+start_time = -2.0  # 開始時間
+end_time = 8.0    # 終了時間
+time_duration = end_time - start_time  # [s]
+fps = 30
+steps = int(time_duration * fps) + 1
+interval_ms = 1000 / fps  # 1コマあたりのミリ秒
+t = np.linspace(start_time, end_time, steps)
+t_on, t_off = 0.0, 4.0   # ステップ歪みを加える時刻
+E_inf = 0.5         # 平衡弾性率
+E_i = 3.0           # 瞬間弾性率
+tau = 2.0           # 緩和時間
 stress_on = strain_i * relaxation_modulus(t - t_on, E_inf, E_i, tau)
 stress_off = -strain_i * relaxation_modulus(t - t_off, E_inf, E_i, tau)
 stress_total = stress_on + stress_off
+
+strain = np.where(t < t_on, 0, np.where(t < t_off, strain_i, 0))
 
 # 2. グラフ設定
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 7), sharex=True)
 
 # 軸の固定
-ax1.set_xlim(-2, 10)
+ax1.set_xlim(start_time, end_time)
 ax1.set_ylim(-0.2, 1.5)
 ax1.set_ylabel('Applied Strain, $\epsilon$ /')
 ax1.set_title('Boltzmann Superposition (Strees Relaxation) ($E_i$ = {0:.1f} MPa, $E_\\infty$ = {1:.1f} MPa, $\\tau$ = {2:.1f} s)'.format(E_i, E_inf, tau))
 ax1.grid(True, ls='--')
 line_strain, = ax1.step([], [], where='post', color='blue', lw=2)
 
-ax2.set_xlim(-2, 10)
+ax2.set_xlim(start_time, end_time)
 ax2.set_ylim(-4.5, 4.5)
 ax2.set_xlabel('Time /s')
 ax2.set_ylabel('Stress, $\sigma$ /MPa')
@@ -43,19 +51,19 @@ ax2.legend(loc='upper right')
 
 # 3. 更新関数
 def animate(i):
-    curr_t = t[:i]
-    # 歪み履歴 (t_onで1になり、t_offで0に戻る)
-    strain_history = np.where(curr_t < t_on, 0, np.where(curr_t < t_off, strain_i, 0))
-    line_strain.set_data(curr_t, strain_history)
+    # 歪みデータの更新(t_onで1になり、t_offで0に戻る)
+    line_strain.set_data(t[:i], strain[:i])
     
     line_s_on.set_data(t[:i], stress_on[:i])
     line_s_off.set_data(t[:i], stress_off[:i])
     line_total.set_data(t[:i], stress_total[:i])
     return line_strain, line_s_on, line_s_off, line_total
 
-ani = animation.FuncAnimation(fig, animate, frames=len(t), interval=20, blit=True)
+# アニメーション実行
+ani = animation.FuncAnimation(fig, animate, frames=steps, interval=interval_ms, blit=True)
 
-ani.save('./gif/Boltzmann_relax_pulseStrain.gif', dpi=300)
+savefile = './mp4/Boltzmann_relax_pulseStrain.mp4'
+ani.save(savefile, writer='ffmpeg', fps=fps, extra_args=['-r', '30'])
 
 plt.tight_layout()
 plt.show()
