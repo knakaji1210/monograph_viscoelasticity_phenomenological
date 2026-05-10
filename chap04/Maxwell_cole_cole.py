96e788bfade481351fdb1b858b3158b395cd1ba9
“knakaji” <knakaji@mac.titech.ac.jp>
2026-05-10 22:39:09 +0900

    260510

---
+++ chap04/Maxwell_cole_cole.py
@@ -0,0 +1,26 @@
+# Cole-Cole plot for Maxwell model
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+xi_min, xi_max = -2, 2
+
+xi = np.logspace(xi_min, xi_max, 200)
+strMod = xi**2 / (1 + xi**2)
+losMod = xi / (1 + xi**2)
+
+fig = plt.figure(figsize=(8,8), tight_layout=True)
+ax = fig.add_subplot(111)
+ax.set_title('Cole-Cole plot for Maxwell model')
+ax.set_xlim(-0.05*np.max(strMod), 1.05*np.max(strMod))
+ax.set_ylim(-0.05*np.max(strMod), 1.05*np.max(strMod))
+ax.set_xlabel(r'$E^{\prime}$ / $E_i$')
+ax.set_ylabel(r'$E^{{\prime\prime}}$ / $E_i$')
+ax.scatter(strMod, losMod)
+ax.grid()
+ax.set_axisbelow(True)
+
+savefile = './png/Maxwell_cole_cole.png'
+fig.savefig(savefile, dpi=300)
+
+plt.show()
