a5255fe6ce513f471a4ba8f7f8946079a6c268a9
“knakaji” <knakaji@mac.titech.ac.jp>
2026-05-17 19:25:59 +0900

    260517

--- chap05_ode/SLS1_sinuStrain.py
+++ chap05_ode/SLS1_sinuStrain.py
@@ -62,8 +62,8 @@ event_time = 0.0    # ステップ歪みを加える時刻
 time_duration = end_time - start_time       # [s] 継続時間
 time_duration_pre = event_time - start_time # [s] ステップ前の継続時間
 time_duration_post = end_time - event_time  # [s] ステップ後の継続時間
-fps = 60            # 1秒あたりのフレーム数、30だと足りないので60に変更
-#fps = 500           # 高周波の場合はfps=500とかしないといけない
+#fps = 60            # 1秒あたりのフレーム数、30だと足りないので60に変更
+fps = 500           # 高周波の場合はfps=500とかしないといけない
 steps = int(time_duration * fps) + 1        # 総フレーム数
 interval_ms = 1000 / fps                    # 1コマあたりのミリ秒
 t = np.linspace(start_time, end_time, steps)
@@ -107,7 +107,7 @@ ax1.set_ylabel('Applied strain, $\epsilon$ /')
 ax1.set_title("SLS I model: sinusoidal strain")
 ax1.grid(True, ls='--')
 
-line_strain, = ax1.plot([], [], color='blue', lw=2, label='Sinusoidal strain')
+line_strain, = ax1.plot([], [], color='blue', lw=2, label='Sinusoidal strain (t = {0:.1f} s)'.format(event_time))
 line_strain_e1, = ax1.plot([], [], color='cyan', ls="dashed", lw=1, label='$\epsilon$ (spring 1)')
 line_strain_e2, = ax1.plot([], [], color='gray', ls="dashed", lw=1, label='$\epsilon$ (Voigt)')
 ax1.legend(loc='upper right')
@@ -119,10 +119,10 @@ ax2.set_xlabel('$t$ /s')
 ax2.set_ylabel('Stress, $\sigma$ /MPa')
 ax2.grid(True, ls='--')
 
-line_stress, = ax2.plot([], [], color='red', lw=2, label='Response to sinusoidal strain (t = {0:.1f} s)'.format(event_time))
+line_stress, = ax2.plot([], [], color='red', lw=2, label='Response to sinusoidal strain')
 line_stress_s1, = ax2.plot([], [], color='green', ls="dashed", lw=1, label='$\sigma$ (spring 2 in Voigt)')
 line_stress_s2, = ax2.plot([], [], color='orange', ls="dashed", lw=1, label='$\sigma$ (dashpot in Voigt)')
-ax2.legend(loc='lower right')
+ax2.legend(loc='upper right')
 
 # テキスト描画
 var_text = r'$\epsilon_{{amp}}$ = {0:.2f}, $E_1$ = {1:.1f} MPa, $E_2$ = {2:.1f} MPa, $\eta$ = {3:.1f} kPa s'.format(eamp,E1/10**6,E2/10**6,eta/10**3)
