37cc4f8baa170b1964e959f16fec4f50b1e19026
“knakaji” <knakaji@mac.titech.ac.jp>
2026-06-06 15:34:37 +0900

    260606

--- chap06/Alfrey_approx_relaxMod.py
+++ chap06/Alfrey_approx_relaxMod.py
@@ -1,27 +1,32 @@
+# Alfrey approximation for relaxation modulus
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 time = 10
-tau_min, tau_max = 0, 40
+tau_min, tau_max = 10**(-3), 40
 
 tau = np.linspace(tau_min, tau_max, 200)
-func = [np.exp(-time/t) for t in tau]
+func = np.exp(-time/tau)
 
 fig = plt.figure(tight_layout=True)
 ax = fig.add_subplot(111)
-ax.set_xlim(tau_min, tau_max)
+ax.set_xlim(0, tau_max)
 ax.set_ylim(-0.1,1.1)
-ax.set_xlabel('tau /s')
-ax.set_ylabel('exp(-t/tau)')
+ax.set_xlabel(r'$\tau$ /s')
+ax.set_ylabel(r'$\exp(-t/\tau)$')
 ax.plot(tau, func)
 ax.hlines([0], tau_min, time, 'r', ls='--')
 ax.hlines([1], time, tau_max, 'r', ls='--')
 ax.hlines([1/np.exp(1)], tau_min, time, 'g', ls='--')
 ax.vlines([time], 0, 1, 'r', ls='--')
 ax.grid()
-fig.text(0.35,0.2, 't = 10 s')
-fig.text(0.075,0.38, '1/e')
+fig.text(0.22,0.86, r'$t$ = 10 s')
+fig.text(0.24,0.22, r'$\tau < t$')
+fig.text(0.35,0.22, r'$t < \tau$')
+fig.text(0.15,0.42, r'1/e')
 
-fig.savefig('./png/approx_form1.png')
+savefile = './png/Alfrey_approx_relaxMod.png'
+fig.savefig(savefile,dpi=300)
 
 plt.show()
--- chap06/Alfrey_approx_strMod.py
+++ chap06/Alfrey_approx_strMod.py
@@ -1,27 +1,31 @@
+# Alfrey approximation for storage modulus
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-freq = 0.1
+angFreq = 0.1
 tau_min, tau_max = 0, 40
 
 tau = np.linspace(tau_min, tau_max, 200)
-func = [freq**2 * t**2/(1 + freq**2 * t**2) for t in tau]
+func = angFreq**2 * tau**2 / (1 + angFreq**2 * tau**2)
 
 fig = plt.figure(tight_layout=True)
 ax = fig.add_subplot(111)
 ax.set_xlim(tau_min, tau_max)
 ax.set_ylim(-0.1,1.1)
-ax.set_xlabel('tau /s')
-ax.set_ylabel('freq^2 * tau^2/(1 + freq^2 * tau^2)')
+ax.set_xlabel(r'$\tau$ /s')
+ax.set_ylabel(r'$\omega^2\tau^2 / (1 + \omega^2\tau^2)$')
 ax.plot(tau, func)
-ax.hlines([0], tau_min, 1/freq, 'r', ls='--')
-ax.hlines([1], 1/freq, tau_max, 'r', ls='--')
-ax.hlines([1/2], tau_min, 1/freq, 'g', ls='--')
-ax.vlines([1/freq], 0, 1, 'r', ls='--')
+ax.hlines([0], tau_min, 1/angFreq, 'r', ls='--')
+ax.hlines([1], 1/angFreq, tau_max, 'r', ls='--')
+ax.hlines([1/2], tau_min, 1/angFreq, 'g', ls='--')
+ax.vlines([1/angFreq], 0, 1, 'r', ls='--')
 ax.grid()
-fig.text(0.35,0.2, '1/freq = 10 s')
-fig.text(0.075,0.48, '0.5')
+fig.text(0.18,0.8, r'1/$\omega$ = 10 s')
+fig.text(0.24,0.22, r'$\tau < t$')
+fig.text(0.35,0.22, r'$t < \tau$')
+fig.text(0.15,0.56, '0.5')
 
-fig.savefig('./png/approx_form2.png')
+fig.savefig('./png/Alfrey_approx_strMod.png')
 
 plt.show()
