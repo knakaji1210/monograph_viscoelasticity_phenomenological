4244cc16d1169d50f10008856f62fded6b74e796
“knakaji” <knakaji@mac.titech.ac.jp>
2026-06-07 16:21:16 +0900

    260607

--- chap06/Alfrey_approx_relaxSpectrum_timeDomain.py
+++ chap06/Alfrey_approx_relaxSpectrum_timeDomain.py
@@ -1,25 +1,27 @@
+# Alfrey approximation example of relaxation spectrum in time domain for Maxwell model
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-T = 10
+tauM = 10
 tau_min, tau_max = -3, 4
 
 tau = np.logspace(tau_min, tau_max, 100)
-print(tau)
-log_tau = [np.log10(t) for t in tau]
-approxRelax = [t*np.exp(-t/T)/T for t in tau]
+log_tau = np.log10(tau)
+approxRelax = tau * np.exp(-tau/tauM) / tauM
 
 fig = plt.figure(tight_layout=True)
 ax = fig.add_subplot(111)
 ax.set_xlim(tau_min, tau_max)
 ax.set_ylim(-0.1,1.1)
-ax.set_xlabel('log(tau /s)')
-ax.set_ylabel('H(tau)')
+ax.set_xlabel(r'log($\tau$ /s)')
+ax.set_ylabel(r'$H(\tau) / E_i$')
 ax.plot(log_tau, approxRelax)
-ax.vlines([np.log10(T)], 0, 1.1, 'r', ls='--')
+ax.vlines([np.log10(tauM)], 0, 1.1, 'r', ls='--')
 ax.grid()
-fig.text(0.2,0.3, 'tau_M = {0} s'.format(T))
+fig.text(0.2,0.8, r'$\tau_M$ = {0:.1f} s'.format(tauM))
 
-fig.savefig('./png/approx_relax_spectrum.png')
+savefile = './png/Alfrey_approx_relaxSpectrum_timeDomain.png'
+fig.savefig(savefile, dpi=300)
 
 plt.show()
