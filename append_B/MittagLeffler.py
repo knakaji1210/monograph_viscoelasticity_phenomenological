0ee45a47bbdb9d64a6e32204a276a37f50367cce
“knakaji” <knakaji@mac.titech.ac.jp>
2026-07-05 00:01:23 +0900

    260705

--- append_B/MittagLeffler.py
+++ append_B/MittagLeffler.py
@@ -11,7 +11,7 @@ from differintP.functions import MittagLeffler
 # 組み込み関数でも「ignore_special_cases=True」を指定すればできることがわかった
 # が、せっかくなので残す
 def MittagLeffler_self(z, terms):
-    """パラメータ alpha=2, beta=1 の Mittag-Leffler 関数を級数和で計算"""
+    """パラメータ a=2, b=1 の Mittag-Leffler 関数を級数和で計算"""
     result = np.zeros_like(z, dtype=float)
     for k in range(terms):
         # 1 / Gamma(2k + 1) = 1 / (2k)!
@@ -38,60 +38,60 @@ if __name__=='__main__':
     t_vals = np.linspace(ts, te, n)
     
     if select == 0:     # exp(x)の場合
-        alpha = 1.0  # Mittag-Leffler関数のパラメータ
-        beta = 1.0   # Mittag-Leffler関数のパラメータ
+        a = 1.0  # Mittag-Leffler関数のパラメータ
+        b = 1.0   # Mittag-Leffler関数のパラメータ
         # X軸のデータ点を生成        
         x_vals = t_vals  # exp(x)のためにt=xとする        
         # 各計算の実行
         y_orig = np.exp(t_vals)
-        y_ML = MittagLeffler(alpha, beta, x_vals, num_terms=50)
+        y_ML = MittagLeffler(a, b, x_vals, num_terms=50)
         savefile = './png/MittagLeffler_exp.png'
     elif select == 1:   # cosh(x)の場合
-        alpha = 2.0  # Mittag-Leffler関数のパラメータ
-        beta = 1.0   # Mittag-Leffler関数のパラメータ
+        a = 2.0  # Mittag-Leffler関数のパラメータ
+        b = 1.0   # Mittag-Leffler関数のパラメータ
         # X軸のデータ点を生成
         x_vals = t_vals**2  # cosh(x)のためにx=t^2とする
         # 各計算の実行
         y_orig = np.cosh(t_vals)
-        y_ML = MittagLeffler(alpha, beta, x_vals, num_terms=50)
+        y_ML = MittagLeffler(a, b, x_vals, num_terms=50)
         savefile = './png/MittagLeffler_cosh.png'
     elif select == 2:   # sinh(x)/xの場合
-        alpha = 2.0  # Mittag-Leffler関数のパラメータ
-        beta = 2.0   # Mittag-Leffler関数のパラメータ
+        a = 2.0  # Mittag-Leffler関数のパラメータ
+        b = 2.0   # Mittag-Leffler関数のパラメータ
         # X軸のデータ点を生成
         x_vals = t_vals**2  # sinh(x)/xのためにx=t^2とする
         # 各計算の実行
         y_orig = np.sinh(t_vals) / t_vals
-        y_ML = MittagLeffler(alpha, beta, x_vals, num_terms=50)
+        y_ML = MittagLeffler(a, b, x_vals, num_terms=50)
         savefile = './png/MittagLeffler_hypersinc.png'
     elif select == 3:   # error関数の場合
-        alpha = 0.5  # Mittag-Leffler関数のパラメータ
-        beta = 1.0   # Mittag-Leffler関数のパラメータ
+        a = 0.5  # Mittag-Leffler関数のパラメータ
+        b = 1.0   # Mittag-Leffler関数のパラメータ
         # X軸のデータ点を生成
         x_vals = np.sqrt(t_vals)    # error関数のためにx=sqrt(t)とする
         # 各計算の実行
         y_orig = np.exp(t_vals) * (1 + special.erf(np.sqrt(t_vals)))
-        y_ML = MittagLeffler(alpha, beta, x_vals, num_terms=50)
+        y_ML = MittagLeffler(a, b, x_vals, num_terms=50)
         savefile = './png/MittagLeffler_error.png'
     elif select == 4:   # cos(x)の場合
-        alpha = 2.0  # Mittag-Leffler関数のパラメータ
-        beta = 1.0   # Mittag-Leffler関数のパラメータ
+        a = 2.0  # Mittag-Leffler関数のパラメータ
+        b = 1.0   # Mittag-Leffler関数のパラメータ
         # X軸のデータ点を生成
         x_vals = -t_vals**2  # cos(x)のためにx=-t^2とする
         # 各計算の実行
         # 組み込み関数で計算（ignore_special_cases=Trueを指定、指定しないとエラーが出る）
         y_orig = np.cos(t_vals)
-        y_ML = MittagLeffler(alpha, beta, x_vals, num_terms=50, ignore_special_cases=True)
+        y_ML = MittagLeffler(a, b, x_vals, num_terms=50, ignore_special_cases=True)
 #        y_ML = MittagLeffler_self(x_vals, terms=50)  # 自作関数（MittagLeffler_self）で計算
         savefile = './png/MittagLeffler_cos.png'
     elif select == 5:   # sin(x)の場合
-        alpha = 2.0  # Mittag-Leffler関数のパラメータ
-        beta = 2.0   # Mittag-Leffler関数のパラメータ
+        a = 2.0  # Mittag-Leffler関数のパラメータ
+        b = 2.0   # Mittag-Leffler関数のパラメータ
         # X軸のデータ点を生成
         x_vals = -t_vals**2  # sin(x)のためにx=-t^2とする
         # 各計算の実行
         y_orig = np.sin(t_vals)
-        y_ML = t_vals * MittagLeffler(alpha, beta, x_vals, num_terms=50, ignore_special_cases=True)
+        y_ML = t_vals * MittagLeffler(a, b, x_vals, num_terms=50, ignore_special_cases=True)
         savefile = './png/MittagLeffler_sin.png'
     else:
         pass
@@ -105,14 +105,14 @@ if __name__=='__main__':
     # グラフの描画
     fig = plt.figure(figsize=(8,5), tight_layout=True)
     ax = fig.add_subplot(111)
-    ax.set_title(r'Mittag-Leffler Function, $E_{\alpha, \beta}(t)$', fontsize=14)
+    ax.set_title(r'Mittag-Leffler Function, $E_{a, b}(t)$', fontsize=14)
     ax.grid()
     ax.set_xlim(0, te)
     ax.set_ylim(ylim[0], ylim[1])
-    ax.plot(t_vals, y_ML, label=f"Mittag-Leffler Function, $\\alpha={alpha}$, $\\beta={beta}$", color="gray", linestyle="-")
+    ax.plot(t_vals, y_ML, label=f"Mittag-Leffler Function, $a={a}$, $b={b}$", color="gray", linestyle="-")
     ax.plot(t_vals, y_orig, label=f"Equivalent Function, $f(t)$ = {func_list[select]}", color="greenyellow", linestyle="--")
     ax.set_xlabel(r"$t$", fontsize=12)
-    ax.set_ylabel(r"$E_{\alpha, \beta}(t)$", fontsize=12)
+    ax.set_ylabel(r"$E_{a, b}(t)$", fontsize=12)
     ax.grid(True, linestyle=":", alpha=0.6)
     ax.legend(fontsize=11)
 
--- append_B/MittagLeffler_multi.py
+++ append_B/MittagLeffler_multi.py
@@ -11,8 +11,8 @@ cmap = plt.get_cmap('Pastel1')
 
 if __name__=='__main__':
     func_list = [r'$e^t$', r'$\cosh(t)$', r'$\sinh(t) / t$', r'$e^t (1 + \mathrm{erf}(\sqrt{t}))$']  # 関数のリスト
-    alpha = [1.0, 2.0, 2.0, 0.5]  # Mittag-Leffler関数のパラメータ alpha のリスト
-    beta = [1.0, 1.0, 2.0, 1.0]  # Mittag-Leffler関数のパラメータ beta のリスト
+    a = [1.0, 2.0, 2.0, 0.5]  # Mittag-Leffler関数のパラメータ a のリスト
+    b = [1.0, 1.0, 2.0, 1.0]  # Mittag-Leffler関数のパラメータ b のリスト
 
     # 計算条件の設定
     ts = 10**(-5)   # 下限（0にすると0除算が起こる関数があるので）
@@ -27,35 +27,35 @@ if __name__=='__main__':
     # exp(x)
     x[0] = t_vals  # exp(x)のためにt=xとする
     y_orig[0] = np.exp(t_vals)
-    y_ML[0] = MittagLeffler(alpha[0], beta[0], x[0], num_terms=50)   
+    y_ML[0] = MittagLeffler(a[0], b[0], x[0], num_terms=50)   
 
     # cosh(x)
     x[1] = t_vals**2  # cosh(x)のためにx=t^2とする
     y_orig[1] = np.cosh(t_vals)
-    y_ML[1] = MittagLeffler(alpha[1], beta[1], x[1], num_terms=50)
+    y_ML[1] = MittagLeffler(a[1], b[1], x[1], num_terms=50)
 
     # sinh(x)/x
     x[2] = t_vals**2  # sinh(x)/xのためにx=t^2とする
     y_orig[2] = np.sinh(t_vals) / t_vals
-    y_ML[2] = MittagLeffler(alpha[2], beta[2], x[2], num_terms=50)
+    y_ML[2] = MittagLeffler(a[2], b[2], x[2], num_terms=50)
 
     # error関数
     x[3] = np.sqrt(t_vals)    # error関数のためにx
-    y_ML[3] = MittagLeffler(alpha[3], beta[3], x[3], num_terms=50)
+    y_ML[3] = MittagLeffler(a[3], b[3], x[3], num_terms=50)
     y_orig[3] = np.exp(t_vals) * (1 + special.erf(np.sqrt(t_vals)))
         
     # グラフの描画
     fig = plt.figure(figsize=(8,5), tight_layout=True)
     ax = fig.add_subplot(111)
-    ax.set_title(r'Mittag-Leffler Function, $E_{\alpha, \beta}(t)$', fontsize=14)
+    ax.set_title(r'Mittag-Leffler Function, $E_{a, b}(t)$', fontsize=14)
     ax.grid()
     ax.set_xlim(0, te)
     ax.set_ylim(0, 10)
     for i in range(4):
-        ax.plot(t_vals, y_ML[i], label=f"Mittag-Leffler Function, $\\alpha={alpha[i]}$, $\\beta={beta[i]}$", color="gray", linestyle="-")
+        ax.plot(t_vals, y_ML[i], label=f"Mittag-Leffler Function, $a={a[i]}$, $b={b[i]}$", color="gray", linestyle="-")
         ax.plot(t_vals, y_orig[i], label=f"Equivalent Function, {func_list[i]}", color=cmap(i/4), linestyle="--")
     ax.set_xlabel(r"$t$", fontsize=12)
-    ax.set_ylabel(r"$E_{\alpha, \beta}(t)$", fontsize=12)
+    ax.set_ylabel(r"$E_{a, b}(t)$", fontsize=12)
     ax.grid(True, linestyle=":", alpha=0.6)
     ax.legend(fontsize=11)
 
