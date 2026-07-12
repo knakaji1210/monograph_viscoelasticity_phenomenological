12e50e6f3e0adc0ccb367111bf4741522571f3ce
Ken Nakajima <knakaji@knakaji-MacBook-Neo.local>
2026-07-12 17:34:57 +0900

    260712

--- append_B/Levy_stable_distribution2.py
+++ append_B/Levy_stable_distribution2.py
@@ -11,10 +11,10 @@ x = np.linspace(-5, 5, 1000)
 params = [
     (2.0, 0.0),  # 正規分布
     (1.0, 0.0),  # コーシー分布
-    (0.5, 1.0)   # (狭義)レヴィ分布
+    (1.5, 0.0)   # 対称レヴィ分布
 ]
 
-func_list = [r'Gaussian ($\alpha=2.0, \beta=0.0$)', r'Cauchy ($\alpha=1.0, \beta=0.0$)', r'Levy ($\alpha=0.5, \beta=1.0$)']  # 関数のリスト
+func_list = [r'Gaussian ($\alpha=2.0, \beta=0.0$)', r'Cauchy ($\alpha=1.0, \beta=0.0$)', r'Levy ($\alpha=1.5, \beta=0.0$)']  # 関数のリスト
 
 # グラフの設定
 fig = plt.figure(figsize=(8,5), tight_layout=True)
@@ -31,11 +31,11 @@ ax.set_title('Probability Density Function (PDF)', fontsize=14)
 ax.set_xlabel('$x$', fontsize=12)
 ax.set_ylabel(f'$\\rho(x)$', fontsize=12)
 ax.set_xlim(-5, 5)
-ax.set_ylim(0, 0.5)
+ax.set_ylim(0, 0.4)
 ax.legend(fontsize=12)
 ax.grid(True, linestyle='--', alpha=0.7)
 
-savefile = './png/Levy_stable_distribution.png'
+savefile = './png/Levy_stable_distribution2.png'
 # グラフの保存
 fig.savefig(savefile, dpi=300)
 
