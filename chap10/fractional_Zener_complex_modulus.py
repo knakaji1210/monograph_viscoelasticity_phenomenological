4417f4fc9d8d5d426115a0670fe6858961277701
“knakaji” <knakaji@mac.titech.ac.jp>
2026-08-16 12:56:59 +0900

    260816

---
+++ chap10/fractional_Zener_complex_modulus.py
@@ -0,0 +1,219 @@
+# complex modulus of fractional Zener model
+ 
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import os
+
+def reqParams():
+    try:
+        insMod = float(input('Enter instantaneous modulus value (GPa) (default = 1 GPa): '))*10**9
+    except ValueError:
+        insMod = 10**9
+    try:
+        infMod = float(input('Enter equilibrium modulus value (MPa) (default = 1 MPa): '))*10**6
+    except ValueError:
+        infMod = 10**6
+    try:
+        modulus = float(input('Enter modulus value of spring-pot (GPa) (default = 1 GPa): '))*10**9
+    except ValueError:
+        modulus = 10**9
+    try:
+        viscosity = float(input('Enter viscosity value of spring-pot (kPa s) (default = 1000 kPa s): '))*10**3
+    except ValueError:
+        viscosity = 10**6
+    try:
+        nu = float(input('Enter fractional power (0 < nu < 1) (default = 0.5): '))
+    except ValueError:
+        nu = 0.5
+    k = insMod/infMod
+    kappa = modulus / (insMod - infMod)
+    relaxTime = kappa**(1/nu)*viscosity/modulus
+    return insMod, infMod, k, modulus, relaxTime, nu
+
+def freqAxes(retardTime):
+    centerAngFreq = 1 / retardTime
+    angFreq = np.logspace(int(np.log10(centerAngFreq))-9, int(np.log10(centerAngFreq))+5, 57)
+    scaledAngFreq = angFreq*retardTime
+    freqAxes = [angFreq, scaledAngFreq]
+    return freqAxes
+
+def calc_complexMod(E, k, tau, af, nu):
+    numer = E*(1/k + (tau*af*(2j/2))**nu)
+    # E must be insMod
+    denom = 1 + (tau*af*(2j/2))**nu
+    comMod = numer/denom
+    strMod = comMod.real
+    losMod = comMod.imag
+    return strMod, losMod
+
+def complexMod(E, k, tau, angFreq, nu):
+    strMod, losMod = calc_complexMod(E, k, tau, angFreq, nu)
+    dynamicMod = [strMod, losMod]
+    return dynamicMod
+
+def fitAngFreqs():
+    try:
+        minAngFreq_s = float(input('Enter minimum frequency for fitting (storage) (default = -4): '))
+    except ValueError:
+        minAngFreq_s = -4
+    try:
+        maxAngFreq_s = float(input('Enter maximum frequency for fitting (storage) (default = -1): '))
+    except ValueError:
+         maxAngFreq_s = -1
+    try:
+        minAngFreq_l = float(input('Enter minimum frequency for fitting (loss) (default = -8): '))
+    except ValueError:
+        minAngFreq_l = -8
+    try:
+        maxAngFreq_l = float(input('Enter maximum frequency for fitting (loss) (default = -2): '))
+    except ValueError:
+         maxAngFreq_l = -2
+    fitFreqs = [minAngFreq_s, maxAngFreq_s, minAngFreq_l, maxAngFreq_l]
+    return fitFreqs
+
+def getNearestIdx(list, num):
+    idx = np.abs(np.asarray(list) - num).argmin()
+    return idx
+
+def fitRegion(angFreq, minNum, maxNum):
+    idx1 = getNearestIdx(angFreq, 10**minNum)
+    idx2 = getNearestIdx(angFreq, 10**maxNum)
+    # インデックスの大小関係を保証し、終端を含めるために +1 する
+    minFit = min(idx1, idx2)
+    maxFit = max(idx1, idx2) + 1
+    fitRegion = [minFit, maxFit]
+    return fitRegion
+
+def loglogFit(x, a, b):
+    return  a*x + b
+
+def fittedArray(x_array, param):
+    fitted_array = [loglogFit(num, param[0], param[1]) for num in x_array]
+    return fitted_array
+
+def curveFit(scaledAngFreq, fitAngFreqs):
+    minFit1, maxFit1 = fitRegion(scaledAngFreq, fitAngFreqs[0], fitAngFreqs[1])
+    minFit2, maxFit2 = fitRegion(scaledAngFreq, fitAngFreqs[2], fitAngFreqs[3])
+    
+    # 1. 貯蔵弾性率（E'）のフィッティング
+    # 選択した範囲のデータがすべて NaN でないかチェック
+    if not np.isnan(log_strMod[minFit1:maxFit1]).all() and (maxFit1 - minFit1) > 1:
+        paramStr,_ = curve_fit(loglogFit, log_scaledAngFreq[minFit1:maxFit1], log_strMod[minFit1:maxFit1])
+        fit_strMod = fittedArray(log_scaledAngFreq, paramStr)
+        fit_result1 = r"$E^\prime \propto (\omega\tau)^{{{0:.2f}}}$".format(paramStr[0])
+    else:
+        # すべて NaN、またはデータ不足の場合はフィッティングをスキップ
+        fit_strMod = np.full_like(log_scaledAngFreq, np.nan)
+        fit_result1 = r"$E^\prime$: Fit Skipped (NaN)"
+
+    # 2. 損失弾性率（E''）のフィッティング
+    # 選択した範囲のデータがすべて NaN でないかチェック (nu=0 の時はここが True になります)
+    if not np.isnan(log_losMod[minFit2:maxFit2]).all() and (maxFit2 - minFit2) > 1:
+        paramLos,_ = curve_fit(loglogFit, log_scaledAngFreq[minFit2:maxFit2], log_losMod[minFit2:maxFit2])
+        fit_losMod = fittedArray(log_scaledAngFreq, paramLos)
+        fit_result2 = r"$E^{{\prime\prime}} \propto (\omega\tau)^{{{0:.2f}}}$".format(paramLos[0])
+    else:
+        # すべて NaN、またはデータ不足の場合はフィッティングをスキップ
+        fit_losMod = np.full_like(log_scaledAngFreq, np.nan)
+        fit_result2 = r"$E^{{\prime\prime}}$: Fit Skipped (NaN)"
+    
+    return fit_strMod, fit_losMod, fit_result1, fit_result2
+
+if __name__=='__main__':
+    # 保存先ディレクトリの作成（存在しない場合のエラー防止）
+    os.makedirs('./png', exist_ok=True)
+
+    # calcul1ating complex modulus and loss tangent
+    insMod, infMod, k, modulus, relaxTime, nu = reqParams()
+    param_text = """
+    ($E_i$ = {0:.1f} MPa, $E_{{\infty}}$ = {1:.1f} MPa, $E$ = {2:.1f} MPa, $\\tau$ = {3:.1f} ms, $\\nu$ = {4:.2f})""".format(insMod/10**6, infMod/10**6, modulus/10**6, relaxTime*10**3, nu)
+    freqAxes = freqAxes(relaxTime)
+    fitting = -1
+    try:
+        select = int(input('Selection (complex modulus (linear): 0, complex modulus (log): 1, loss tangent: 2): '))
+    except ValueError:
+        select = 0
+
+    angFreq = freqAxes[0]
+    scaledAngFreq = freqAxes[1]
+    scaledAngFreq_label = r'log($\omega\tau$)'
+    strMod = complexMod(insMod, k, relaxTime, angFreq, nu)[0]
+    losMod = complexMod(insMod, k, relaxTime, angFreq, nu)[1]
+    losTan = np.divide(losMod, strMod, where=strMod!=0, out=np.full_like(strMod, np.nan))
+    log_scaledAngFreq = np.log10(scaledAngFreq)
+    log_strMod = np.log10(strMod, where=strMod>0, out=np.full_like(strMod, np.nan))
+    log_losMod = np.log10(losMod, where=losMod>0, out=np.full_like(losMod, np.nan))
+
+    if select == 0:
+        y1 = strMod/10**6             # rescale to MPa
+        y2 = losMod/10**6             # rescale to MPa
+        y_label = r'$E^\prime$, $E^{{\prime\prime}}$ /MPa'
+        ylim = [np.nanmax(np.concatenate([y1, y2]))*(-0.2), np.nanmax(np.concatenate([y1, y2]))*1.4]
+        label1 = r'$E^\prime$ (linear)'
+        label2 = r'$E^{{\prime\prime}}$ (linear)'
+        c1 = 'r'
+        c2 = 'b'
+        a = 1
+        legend_loc='upper left'
+        savefile = './png/fractional_Zener_complex_modulus_linear_nu{:.2f}.png'.format(nu)
+
+    if select == 1:
+        y1 = log_strMod
+        y2 = log_losMod
+        y_label = r'log($E^\prime$, $E^{{\prime\prime}}$ /Pa)'
+        ylim = [np.nanmin(np.concatenate([y1, y2]))-2, np.nanmax(np.concatenate([y1, y2]))+2]
+        label1 = r'$E^\prime$ (log)'
+        label2 = r'$E^{{\prime\prime}}$ (log)'
+        c1 = 'r'
+        c2 = 'b'
+        a = 1
+        legend_loc='upper left'
+        savefile = './png/fractional_Zener_complex_modulus_log_nu{:.2f}.png'.format(nu)
+
+        try:
+            fitting = int(input('Selection (curve fit: 0, no curve fit: 1): '))
+        except ValueError:
+            fitting = 0
+        if fitting == 1:
+            pass
+        if fitting == 0:
+            fitAngFreqs = fitAngFreqs()
+            fit_strMod, fit_losMod, fit_result1, fit_result2 = curveFit(scaledAngFreq, fitAngFreqs)
+
+    if select == 2:
+        y1 = losTan
+        y2 = np.zeros(len(y1))
+        y_label = 'tan $\delta$ /'
+        ylim = [-0.2, np.nanmax(y1)*1.2]
+        label1 = r'tan $\delta$'
+        label2 = ''
+        c1 = 'g'
+        c2 = 'b'
+        a = 0
+        legend_loc='upper right'
+        savefile = './png/fractional_Zener_loss_tangent_nu{:.2f}.png'.format(nu)
+
+    # drawing graphs
+    fig = plt.figure(figsize=(8,5), tight_layout=True)
+    ax = fig.add_subplot(111)
+    ax.set_title('fractional Zener model '+param_text)
+    ax.set_xlabel(scaledAngFreq_label)
+    ax.set_ylabel(y_label)
+    ax.scatter(log_scaledAngFreq, y1, c=c1, label=label1)
+    ax.scatter(log_scaledAngFreq, y2, c=c2, label=label2, alpha=a)
+
+    # fittingを実行した（fitting == 0）ときのみプロットする
+    if fitting == 0:
+        ax.plot(log_scaledAngFreq, fit_strMod, c='r', lw=1, ls=':', label=r'fitted $E^{\prime}$')
+        ax.plot(log_scaledAngFreq, fit_losMod, c='b', lw=1, ls=':', label=r'fitted $E^{{\prime\prime}}$')
+        fig.text(0.7, 0.35, fit_result1)
+        fig.text(0.7, 0.30, fit_result2)
+
+    ax.set_ylim(ylim[0], ylim[1])   
+    ax.legend(loc=legend_loc)
+    ax.grid()
+    ax.set_axisbelow(True)
+    fig.savefig(savefile, dpi=300)
+
+    plt.show()
