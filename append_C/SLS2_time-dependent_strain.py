f9dd841d0324b26107e078b77011233549917b1f
“knakaji” <knakaji@mac.titech.ac.jp>
2026-09-02 19:54:52 +0900

    260902

--- append_C/SLS2_time-dependent_strain.py
+++ append_C/SLS2_time-dependent_strain.py
@@ -1,4 +1,4 @@
-# response of SLS1 model to time-dependent stress
+# response of SLS2 model to time-dependent strain
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,34 +14,34 @@ def reqParams():
     except ValueError:
         E2 = 10**6                # [Pa] 弾性率
     try:
-        eta = float(input('viscosity [kPa s] (default = 10.0 kPa s): '))*10**3
+        eta = float(input('viscosity [kPa s] (default = 100.0 kPa s): '))*10**3
     except ValueError:
-        eta = 10*10**3               # [Pa s] 粘度
+        eta = 100*10**3               # [Pa s] 粘度
 
     # パラメータの計算
-    insMod = E1                 # [Pa] 瞬間弾性率
-    infMod = E1*E2/(E1+E2)      # [Pa] 緩和弾性率
-    retardTime = eta/E2          # [s] 緩和時間
+    insMod = E1 + E2             # [Pa] 瞬間弾性率
+    infMod = E2                  # [Pa] 緩和弾性率
+    relaxTime = eta/E1          # [s] 緩和時間
     k = insMod/infMod
 
-    return E1, E2, eta, insMod, infMod, retardTime, k
+    return E1, E2, eta, insMod, infMod, relaxTime, k
 
-def func_SLS1_1(modulus, retardTime, k):
+def func_SLS2_1(modulus, relaxTime, k):
     # modulus must be infMod
     try:
-        c = float(input('Enter c of stress = c*time (default = 0.01 MPa/ms): '))
+        c = float(input('Enter c of strain = c*time (default = 0.01 1/ms): '))
     except ValueError:
-        c = 10**7
+        c = 10
     try:
         t1 = float(input('Enter t1 (0<=t<t1) (ms) (default = 40 ms): '))*10**(-3)
     except ValueError:
         t1 = 40*10**(-3)
     tim = np.linspace(0, t1, 400)
-    stress = c*tim
-    strain = (c/modulus)*(tim - retardTime * (1 - 1/k) * (1 - np.exp(-tim/retardTime)))
-    return c, t1, tim, stress, strain
+    strain = c*tim
+    stress = (c*modulus)*(tim + relaxTime * (k - 1) * (1 - np.exp(-tim/relaxTime)))
+    return c, t1, tim, strain, stress
 
-def func_SLS1_2(modulus, retardTime, c, t1, e1):
+def func_SLS2_2(modulus, relaxTime, c, t1, s1):
     # modulus must be infMod
     try:
         dt = float(input('Enter dt = t2 - t1 (t1<=t<t2) (default = 60 ms): '))*10**(-3)
@@ -49,11 +49,11 @@ def func_SLS1_2(modulus, retardTime, c, t1, e1):
         dt = 60*10**(-3)
     t2 = t1 + dt
     tim = np.linspace(t1, t2, 400)
-    stress = c*t1*np.ones(len(tim))
-    strain = (c/modulus) * (t1 + (e1/(c/modulus) - t1) * np.exp(-(tim - t1)/retardTime))
-    return t2, tim, stress, strain
+    strain = c*t1*np.ones(len(tim))
+    stress = (c*modulus) * (t1 + (s1/(c*modulus) - t1) * np.exp(-(tim - t1)/relaxTime))
+    return t2, tim, strain, stress
 
-def func_SLS1_3(modulus, retardTime, c, t1, t2, e2, k):
+def func_SLS2_3(modulus, relaxTime, c, t1, t2, s2, k):
     # modulus must be infMod
     try:
         dt = float(input('Enter dt = t3 - t2 (t2<=t<t3) (default = 40 ms): '))*10**(-3)  
@@ -61,11 +61,11 @@ def func_SLS1_3(modulus, retardTime, c, t1, t2, e2, k):
         dt = 40*10**(-3)
     t3 = t2 + dt
     tim = np.linspace(t2, t3, 400)
-    stress = c*(t1 + t2 - tim)
-    strain = (c/modulus)*(retardTime * (1 - 1/k) + t1 + t2 - tim + (e2/(c/modulus) - t1 - retardTime * (1 - 1/k)) * np.exp(-(tim-t2)/retardTime))
-    return t3, tim, stress, strain
+    strain = c*(t1 + t2 - tim)
+    stress = (c*modulus)*(relaxTime * (1 - k) + t1 + t2 - tim + (s2/(c*modulus) - t1 - relaxTime * (1 - k)) * np.exp(-(tim-t2)/relaxTime))
+    return t3, tim, strain, stress
 
-def func_SLS1_4(retardTime, t3, e3):
+def func_SLS2_4(relaxTime, t3, s3):
     # modulus must be infMod
     try:
         dt = float(input('Enter dt = t4 - t3 (t3<=t<t4) (default = 40 ms): '))*10**(-3)  
@@ -73,29 +73,30 @@ def func_SLS1_4(retardTime, t3, e3):
         dt = 40*10**(-3)
     t4 = t3 + dt
     tim = np.linspace(t3, t4, 400)
-    stress = np.zeros(len(tim))
-    strain = e3 * np.exp(-(tim-t3)/retardTime)
-    return t4, tim, stress, strain
+    strain = np.zeros(len(tim))
+    stress = s3 * np.exp(-(tim-t3)/relaxTime)
+    return t4, tim, strain, stress
 
 if __name__=='__main__':
-    E1, E2, eta, insMod, infMod, retardTime, k = reqParams()
+    E1, E2, eta, insMod, infMod, relaxTime, k = reqParams()
     param_text = r'($E_1$ = {0:.1f} MPa, $E_2$ = {1:.1f} MPa, $\eta$ = {2:.1f} kPa s)'.format(E1/10**6, E2/10**6, eta/10**3)
-    res_text = r'$E_i$ = {0:.2f} MPa, $E_\infty$ = {1:.2f} MPa, $k$ = {2:.2f}, $\tau$ = {3:.2f} ms'.format(insMod/10**6, infMod/10**6, k, retardTime*10**3)
-    c, t1, tim1, stress1, strain1 = func_SLS1_1(infMod, retardTime, k)
-    e1 = strain1[-1]    # strain at t1
-    t2, tim2, stress2, strain2 = func_SLS1_2(infMod, retardTime, c, t1, e1)
-    e2 = strain2[-1]    # strain at t2
-    t3, tim3, stress3, strain3 = func_SLS1_3(infMod, retardTime, c, t1, t2, e2, k)
-    e3 = strain3[-1]    # strain at t3
-    t4, tim4, stress4, strain4 = func_SLS1_4(retardTime, t3, e3)
+    res_text = r'$E_i$ = {0:.2f} MPa, $E_\infty$ = {1:.2f} MPa, $k$ = {2:.2f}, $\tau$ = {3:.2f} ms'.format(insMod/10**6, infMod/10**6, k, relaxTime*10**3)
+    c, t1, tim1, strain1, stress1 = func_SLS2_1(infMod, relaxTime, k)
+    s1 = stress1[-1]    # stress at t1
+    t2, tim2, strain2, stress2 = func_SLS2_2(infMod, relaxTime, c, t1, s1)
+    s2 = stress2[-1]    # stress at t2
+    t3, tim3, strain3, stress3 = func_SLS2_3(infMod, relaxTime, c, t1, t2, s2, k)
+    s3 = stress3[-1]    # stress at t3
+    t4, tim4, strain4, stress4 = func_SLS2_4(relaxTime, t3, s3)
 
     tim0 = np.linspace(-t1, 0, 400)
     stress0 = np.zeros(len(tim0))
     strain0 = np.zeros(len(tim0))
 
     tim = np.concatenate([tim0,tim1,tim2,tim3,tim4])/10**(-3)   # rescale to ms
-    stress = np.concatenate([stress0,stress1,stress2,stress3,stress4])/10**6   # rescale to MPa
     strain = np.concatenate([strain0,strain1,strain2,strain3,strain4])
+    stress = np.concatenate([stress0,stress1,stress2,stress3,stress4])/10**6   # rescale to MPa
+
 
     try:
         select = int(input('Selection (strain&stress vs time: 0, stress vs strain: 1): '))
@@ -105,42 +106,42 @@ if __name__=='__main__':
     if select == 0:
         fig = plt.figure(figsize=(8,10), tight_layout=True)
         ax1 = fig.add_subplot(211)
-        ax1.set_title('SLS1 model for time-dependent strain '+param_text)
+        ax1.set_title('SLS2 model for time-dependent strain '+param_text)
         ax1.set_xlabel(r'$t$ /ms')
-        ax1.set_ylabel(r'$\sigma$ /MPa')
+        ax1.set_ylabel(r'$\epsilon$ /')
         ax1.set_xlim(-t1*10**3, t4*10**3)
-        ax1.set_ylim(-0.05*np.max(stress), 1.2*np.max(stress))
+        ax1.set_ylim(-0.05*np.max(strain), 1.2*np.max(strain))
         ax1.grid()
         ax1.set_axisbelow(True)
         ax1.text(0.05, 0.95, res_text, transform=ax1.transAxes, verticalalignment='top')
-        ax1.plot(tim, stress, c='r', lw=2, label='Time-dependent stress')
+        ax1.plot(tim, strain, c='b', lw=2, label='Time-dependent strain')
         ax1.legend(loc='upper right')
 
         ax2 = fig.add_subplot(212)
         ax2.set_xlabel(r'$t$ /ms')
-        ax2.set_ylabel(r'$\epsilon$ /')
+        ax2.set_ylabel(r'$\sigma$ /MPa')
         ax2.set_xlim(-t1*10**3, t4*10**3)
-        ax2.set_ylim(-0.05*np.max(strain), 1.2*np.max(strain))
+        ax2.set_ylim(1.2*np.min(stress), 1.2*np.max(stress))
         ax2.grid()
         ax2.set_axisbelow(True)
-        ax2.plot(tim, strain, c='b', lw=2, label='Response to time-dependent stress')
+        ax2.plot(tim, stress, c='r', lw=2, label='Response to time-dependent strain')
         ax2.legend(loc='upper right')
-        savefile = './png/SLS1_time-dependent_strain_(tau={0:.1f}ms).png'.format(retardTime*10**3)
+        savefile = './png/SLS2_time-dependent_strain_(tau={0:.1f}ms).png'.format(relaxTime*10**3)
 
     elif select == 1:
         fig = plt.figure(figsize=(8,5), tight_layout=True)
         ax1 = fig.add_subplot(111)
-        ax1.set_title('SLS1 model for time-dependent strain '+param_text)
+        ax1.set_title('SLS2 model for time-dependent strain '+param_text)
         ax1.set_xlabel(r'$\epsilon$ /')
         ax1.set_ylabel(r'$\sigma$ /MPa')
         ax1.set_xlim(-0.05*np.max(strain), 1.2*np.max(strain))
-        ax1.set_ylim(-0.05*np.max(stress), 1.2*np.max(stress))
+        ax1.set_ylim(1.2*np.min(stress), 1.2*np.max(stress))
         ax1.grid()
         ax1.set_axisbelow(True)
         ax1.text(0.05, 0.95, res_text, transform=ax1.transAxes, verticalalignment='top')
         ax1.plot(strain, stress, c='r', lw=2, label='stress-strain curve')
         ax1.legend(loc='upper right')
-        savefile = './png/SLS1_stress-strain_curve_(tau={0:.1f}ms).png'.format(retardTime*10**3)
+        savefile = './png/SLS2_stress-strain_curve_(tau={0:.1f}ms).png'.format(relaxTime*10**3)
 
     fig.savefig(savefile, dpi=300)
     plt.show()
